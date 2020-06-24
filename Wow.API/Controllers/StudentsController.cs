08c99890d749c42facc0eacd70083e86b01c8702
Kosta Petan <56195950+kopetan-ms@users.noreply.github.com>
2020-06-24 14:08:54 +0200

    Add redis cache

--- Wow.API/Controllers/StudentsController.cs
+++ Wow.API/Controllers/StudentsController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using Wow.API.Models;
 
@@ -15,11 +17,14 @@ namespace Wow.API.Controllers
     public class StudentsController : ControllerBase
     {
         private readonly SchoolContext _context;
+        private readonly IDistributedCache cache;
         private readonly ILogger<StudentsController> _logger;
+        private readonly static string Key = "Key";
 
-        public StudentsController(SchoolContext context)
+        public StudentsController(SchoolContext context, IDistributedCache cache)
         {
             _context = context;
+            this.cache = cache;
         }
 
         [HttpGet]
@@ -28,5 +33,24 @@ namespace Wow.API.Controllers
             var students = await _context.Students.ToListAsync();
             return Ok(students);
         }
+
+        [HttpGet("cacheset")]
+        public async Task<IActionResult> SetCache()
+        {
+            var currentTimeUTC = DateTime.UtcNow.ToString();
+            var result = await cache.GetStringAsync(Key);
+            await cache.SetStringAsync(Key, $"{result} \n {currentTimeUTC}");
+            return Ok();
+        }
+
+        [HttpGet("cacheget")]
+        public async Task<IActionResult> GetCache()
+        {
+            
+            var result = await cache.GetStringAsync(Key);
+            return Ok(result);
+        }
+
+
     }
 }
--- Wow.API/Startup.cs
+++ Wow.API/Startup.cs
@@ -28,8 +28,20 @@ namespace Wow.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddDbContext<SchoolContext>(options =>
-                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                        providerOptions => providerOptions.EnableRetryOnFailure()
+                    );
+                
+            });
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetConnectionString("RedisConnection");
+                options.InstanceName = "Wow.Redis";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
