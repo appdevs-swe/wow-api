ea389b349d35895621f8ffeb61f5163d75f65925
Kosta Petan <56195950+kopetan-ms@users.noreply.github.com>
2020-07-06 17:30:04 +0200

    refactor

---
+++ Wow.API/Controllers/CacheController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace Wow.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDistributedCache cache;
+        private readonly ILogger<CacheController> _logger;
+
+        public CacheController(IDistributedCache cache)
+        {
+            this.cache = cache;
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Set([FromBody] CacheItem item)
+        {
+            _logger.LogInformation($"Setting key:{item.Key} value:{item.Value} in cache");
+            await cache.SetStringAsync(item.Key, item.Value);
+            return Ok();
+        }
+
+        [HttpGet("{key}")]
+        public async Task<IActionResult> Get([FromRoute]string key)
+        {
+            _logger.LogInformation($"Getting key:{key} from cache");
+            var result = await cache.GetStringAsync(key);
+            return Ok(result);
+        }
+    }
+
+    public class CacheItem
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
--- Wow.API/Controllers/StudentsController.cs
+++ Wow.API/Controllers/StudentsController.cs
@@ -17,40 +17,19 @@ namespace Wow.API.Controllers
     public class StudentsController : ControllerBase
     {
         private readonly SchoolContext _context;
-        private readonly IDistributedCache cache;
         private readonly ILogger<StudentsController> _logger;
-        private readonly static string Key = "Key";
 
-        public StudentsController(SchoolContext context, IDistributedCache cache)
+        public StudentsController(SchoolContext context)
         {
             _context = context;
-            this.cache = cache;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
+            _logger.LogInformation($"Getting students from DB");
             var students = await _context.Students.ToListAsync();
             return Ok(students);
         }
-
-        [HttpGet("cacheset")]
-        public async Task<IActionResult> SetCache()
-        {
-            var currentTimeUTC = DateTime.UtcNow.ToString();
-            var result = await cache.GetStringAsync(Key);
-            await cache.SetStringAsync(Key, $"{result} \n {currentTimeUTC}");
-            return Ok();
-        }
-
-        [HttpGet("cacheget")]
-        public async Task<IActionResult> GetCache()
-        {
-            
-            var result = await cache.GetStringAsync(Key);
-            return Ok(result);
-        }
-
-
     }
 }
